636f05420c4cd0daf3f538e194a1a35a756c2f49
Alex Tideman <alex.tideman@gmail.com>
2017-01-20 14:00:59 -0700

    Nothing important here

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const md5 = require('md5');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,8 +17,10 @@ app.locals.secrets = {
 app.get('/', (request, response) => {
   response.send('It\'s a secret to everyone.');
 });
+//
 
 app.get('/api/secrets/:id', (request, response) => {
+  console.log(request.params)
   const { id } = request.params;
   const message = app.locals.secrets[id];
 
@@ -40,6 +44,31 @@ app.post('/api/secrets', (request, response) => {
   response.status(201).json({ id, message });
 });
 
+app.put('/api/secrets/:id', (request, response) => {
+  const { id } = request.params;
+  const { message } = request.body;
+  const oldMessage = app.locals.secrets[id];
+
+  if (!oldMessage) { return response.status(404); }
+
+  app.locals.secrets[id] = message;
+
+  response.json({ id, message });
+});
+
+app.delete('/api/secrets/:id', (request, response) => {
+  const { id } = request.params;
+  const oldMessage = app.locals.secrets[id];
+
+  if (!oldMessage) { return response.status(404).send({
+      error: 'There is no message with the "id" of ${id}.'
+    })
+  }
+
+  delete app.locals.secrets[id];
+
+  response.send('Secret deleted');
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
