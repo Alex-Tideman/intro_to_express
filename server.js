0206eeb7deeecc52c2a639b0b8ac2456f4eefba1
Alex Tideman <alex.tideman@gmail.com>
2017-01-24 10:59:15 -0700

    Finished live coding

--- server.js
+++ server.js
@@ -1,75 +1,77 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const bodyParser = require('body-parser');
 const md5 = require('md5');
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Secret Box';
-
+app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Secret Box'
 app.locals.secrets = {
-  wowowow: 'I am a banana'
-};
+  wowow: "I am a banana",
+  food: "I hate mash potatoes",
+  1: "one"
+}
 
 app.get('/', (request, response) => {
-  response.send('It\'s a secret to everyone.');
-});
-//
+  response.send('Its a secret to everyone')
+})
 
-app.get('/api/secrets/:id', (request, response) => {
-  console.log(request.params)
-  const { id } = request.params;
-  const message = app.locals.secrets[id];
+app.get('/home', (request, response) => {
+  response.send('Its a secret to everyone')
+})
 
-  if (!message) { return response.sendStatus(404); }
+app.get('/about', (request, response) => {
+  response.send('Its a secret to everyone')
+})
 
-  response.json({ id, message });
-});
+app.get('/api/secrets', (request, response) => {
+  response.json(app.locals.secrets)
+})
 
 app.post('/api/secrets', (request, response) => {
-  const { message } = request.body;
-  const id = md5(message);
+  const { message } = request.body
+  const id = md5(message)
 
-  if (!message) {
-    return response.status(422).send({
-      error: 'No message property provided'
-    });
+  app.locals.secrets[id] = message
+
+  response.json({ id, message })
+})
+
+
+
+app.get('/api/secrets/:id', (request, response) => {
+  const { id } = request.params
+  const message = app.locals.secrets[id]
+
+  !message ? response.sendStatus(404) : response.json({ id, message })
+})
+
+app.patch('/api/secrets/:id', (request, response) => {
+  const { id } = request.params
+  const { message } = request.body
+
+  if(!app.locals.secrets[id]) {
+    response.sendStatus(404)
   }
 
-  app.locals.secrets[id] = message;
-
-  response.status(201).json({ id, message });
-});
-
-app.put('/api/secrets/:id', (request, response) => {
-  const { id } = request.params;
-  const { message } = request.body;
-  const oldMessage = app.locals.secrets[id];
-
-  if (!oldMessage) { return response.status(404); }
-
-  app.locals.secrets[id] = message;
-
-  response.json({ id, message });
-});
+  app.locals.secrets[id] = message
+  response.json({ id, message })
+})
 
 app.delete('/api/secrets/:id', (request, response) => {
-  const { id } = request.params;
-  const oldMessage = app.locals.secrets[id];
+  const { id } = request.params
 
-  if (!oldMessage) { return response.status(404).send({
-      error: 'There is no message with the "id" of ${id}.'
-    })
+  if(!app.locals.secrets[id]) {
+    response.sendStatus(404)
   }
 
-  delete app.locals.secrets[id];
+  delete app.locals.secrets[id]
+  response.json({ message: "secret deleted" })
+})
 
-  response.send('Secret deleted');
-});
 
 app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-});
+  console.log(`${app.locals.title} is runing on ${app.get('port')}.`)
+})
